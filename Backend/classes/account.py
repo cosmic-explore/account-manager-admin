a804d5507a6432a8997219ac7c4550c5f123e82a
cosmic-explore <nickwactor@gmail.com>
2026-02-02 17:00:41 -0700

    added resources backref to account

--- Backend/classes/account.py
+++ Backend/classes/account.py
@@ -1,10 +1,12 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import db
 
 
 class Account(db.Model):
     name: Mapped[str] = mapped_column(nullable=False, unique=True)
     status: Mapped[str] = mapped_column(nullable=False)
+
+    resources = relationship("Resource", back_populates="account")
 
     def __init__(self, name, status):
         self.name = name
