c08e4e02175a612e0a5aa0f1acf17675c9e3c1fb
cosmic-explore <nickwactor@gmail.com>
2026-02-08 14:29:42 -0700

    add auth/me route to backend and fix allowed http methods for login/logout

--- Backend/routes/auth.py
+++ Backend/routes/auth.py
@@ -2,17 +2,16 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-from flask import request, Response, Blueprint
+from flask import jsonify, request, Response, Blueprint
 from flask_login import login_required, logout_user, current_user, login_user
 
-# from app import login_manager
 from classes.person import Person
 from services.persons import validate_person
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@auth_bp.route("/login", methods=["GET", "POST"])
+@auth_bp.route("/login", methods=["POST"])
 def login():
     if current_user.is_authenticated:
         return Response(status=418)
@@ -28,9 +27,16 @@ def login():
         return Response(status=204)
 
 
-@auth_bp.route("/logout", methods=["GET"])
+@auth_bp.route("/logout", methods=["POST"])
 @login_required
 def logout():
     logging.info(f"LOGOUT {current_user.email}")
     logout_user()
     return Response(status=204)
+
+
+@auth_bp.route("/me", methods=["GET"])
+@login_required
+def me():
+    user_data = {"email": current_user.email, "role": current_user.role}
+    return jsonify(user_data)
