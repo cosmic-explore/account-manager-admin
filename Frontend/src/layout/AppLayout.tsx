820d45c9f08b8c5cdf4e0d688d04ee39e30409be
cosmic-explore <nickwactor@gmail.com>
2026-02-09 14:54:36 -0700

    add logout button

--- Frontend/src/layout/AppLayout.tsx
+++ Frontend/src/layout/AppLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import { Navbar } from '../features/navigation/Navbar'
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
+import { useAuth } from '../features/auth/UseAuth'
 
 export const AppLayout = () => {
+    const { logout } = useAuth()
+
     return (
         <Box sx={{ display: 'flex', minHeight: '100vh', width: '100%' }}>
             <Box sx={{ width: 240 }}>
@@ -10,6 +13,11 @@ export const AppLayout = () => {
             </Box>
             <Box sx={{ flex: 1, display: 'flex', p: '2rem' }}>
                 <Outlet />
+            </Box>
+            <Box sx={{ p: '2rem' }}>
+                <Button variant="text" onClick={() => logout()}>
+                    Logout
+                </Button>
             </Box>
         </Box>
     )
