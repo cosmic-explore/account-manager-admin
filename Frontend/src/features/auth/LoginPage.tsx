10f40ff9298ce56a621d627c28b3ff0a73c28537
cosmic-explore <nickwactor@gmail.com>
2026-02-09 14:57:45 -0700

    fix width of login page

--- Frontend/src/features/auth/LoginPage.tsx
+++ Frontend/src/features/auth/LoginPage.tsx
@@ -1,17 +1,19 @@
-import { Typography } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import { LoginForm } from './LoginForm'
 
 export const LoginPage = () => {
     return (
-        <Card variant="outlined">
-            <CardContent>
-                <Typography variant="h5" align="left">
-                    Sign in
-                </Typography>
-                <LoginForm />
-            </CardContent>
-        </Card>
+        <Container maxWidth="sm">
+            <Card variant="outlined">
+                <CardContent>
+                    <Typography variant="h5" align="left">
+                        Sign in
+                    </Typography>
+                    <LoginForm />
+                </CardContent>
+            </Card>
+        </Container>
     )
 }
