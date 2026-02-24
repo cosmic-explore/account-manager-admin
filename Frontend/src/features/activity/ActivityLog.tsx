f4ad428f2d93eb7809038dbe4e0cd4c67ad2a4d5
cosmic-explore <nickwactor@gmail.com>
2026-02-24 15:03:38 -0700

    change prop hierarchy for Activity page

--- Frontend/src/features/activity/ActivityLog.tsx
+++ Frontend/src/features/activity/ActivityLog.tsx
@@ -1,20 +1,8 @@
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material'
 import type { ActivityInfo } from '../../types/activities'
 import { ActivityTableRow } from './ActivityTableRow'
-import { useEffect, useState } from 'react'
-import { requestActivities } from '../../api/activities'
 
-export const ActivityLog = () => {
-    const [activityList, setActivityList] = useState<ActivityInfo[]>([])
-
-    useEffect(() => {
-        requestActivities().then(response => {
-            const activities = response as ActivityInfo[]
-            activities.sort((a, b) => (new Date(a.timestamp) > new Date(b.timestamp) ? -1 : 1))
-            setActivityList(activities)
-        })
-    }, [])
-
+export const ActivityLog = (props: { activityList: ActivityInfo[] }) => {
     return (
         <Table>
             <TableHead>
@@ -26,7 +14,7 @@ export const ActivityLog = () => {
                 </TableRow>
             </TableHead>
             <TableBody>
-                {activityList.map(activity => (
+                {props.activityList.map(activity => (
                     <ActivityTableRow key={activity.id} {...activity} />
                 ))}
             </TableBody>
--- Frontend/src/features/activity/ActivityPage.tsx
+++ Frontend/src/features/activity/ActivityPage.tsx
@@ -1,14 +1,27 @@
 import { Typography } from '@mui/material'
 import { DefaultPage } from '../../layout/DefaultPage'
 import { ActivityLog } from './ActivityLog'
+import { useEffect, useState } from 'react'
+import type { ActivityInfo } from '../../types/activities'
+import { requestActivities } from '../../api/activities'
 
 export const ActivitiesPage = () => {
+    const [activityList, setActivityList] = useState<ActivityInfo[]>([])
+
+    useEffect(() => {
+        requestActivities().then(response => {
+            const activities = response as ActivityInfo[]
+            activities.sort((a, b) => (new Date(a.timestamp) > new Date(b.timestamp) ? -1 : 1))
+            setActivityList(activities)
+        })
+    }, [])
+
     return (
         <DefaultPage>
             <Typography variant="h4" sx={{ pb: '1rem' }}>
                 Platform Activity
             </Typography>
-            <ActivityLog />
+            <ActivityLog activityList={activityList} />
         </DefaultPage>
     )
 }
