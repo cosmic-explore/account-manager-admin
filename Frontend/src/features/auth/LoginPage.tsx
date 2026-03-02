18ff665f424ec955795fe6039ddd4e1c01fc4681
cosmic-explore <nickwactor@gmail.com>
2026-03-02 11:49:49 -0700

    add title to login page

--- Frontend/src/features/auth/LoginPage.tsx
+++ Frontend/src/features/auth/LoginPage.tsx
@@ -5,15 +5,20 @@ import { LoginForm } from './LoginForm'
 
 export const LoginPage = () => {
     return (
-        <Container maxWidth="sm">
-            <Card variant="outlined" sx={{ p: '2rem' }}>
-                <CardContent>
-                    <Typography variant="h5" align="left">
-                        Sign in
-                    </Typography>
-                    <LoginForm />
-                </CardContent>
-            </Card>
+        <Container sx={{ minHeight: '100vh' }}>
+            <Typography variant="h2" sx={{ pt: '4rem', pb: '4rem' }}>
+                Account Management Platform
+            </Typography>
+            <Container maxWidth="sm">
+                <Card variant="outlined" sx={{ p: '2rem' }}>
+                    <CardContent>
+                        <Typography variant="h5" align="left">
+                            Sign in
+                        </Typography>
+                        <LoginForm />
+                    </CardContent>
+                </Card>
+            </Container>
         </Container>
     )
 }
