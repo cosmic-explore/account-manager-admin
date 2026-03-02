857f18201096619600e4f822a0b8b96257d967b1
cosmic-explore <nickwactor@gmail.com>
2026-03-02 14:02:24 -0700

    Style Navbar and use MUI drawer

--- Frontend/src/features/navigation/Navbar.tsx
+++ Frontend/src/features/navigation/Navbar.tsx
@@ -1,26 +1,65 @@
-import { Box, ListItemButton, ListItemText } from '@mui/material'
-import { NavLink } from 'react-router-dom'
+import { Drawer, Box, ListItemButton, ListItemText } from '@mui/material'
+import {
+    DashboardOutlined,
+    PeopleAltOutlined,
+    ListOutlined,
+    HandshakeOutlined,
+} from '@mui/icons-material'
+import { NavLink, useLocation } from 'react-router-dom'
 import { AdminOnlyWrapper } from '../../layout/AdminOnlyWrapper'
 
-export const Navbar = () => {
+export const Navbar = (props: { width: number }) => {
+    const location = useLocation()
+
+    const isLocation = (path: string) => {
+        return path == location.pathname
+    }
+
     return (
-        <Box sx={{ p: '2rem' }}>
+        <Drawer
+            sx={{
+                width: props.width,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: 240,
+                },
+            }}
+            variant="permanent"
+            anchor="left"
+        >
+            <Box sx={{ pb: '1rem' }} />
             <nav>
-                <ListItemButton component={NavLink} to="/dashboard">
+                <ListItemButton
+                    component={NavLink}
+                    to="/dashboard"
+                    selected={isLocation('/dashboard')}
+                >
+                    <DashboardOutlined sx={{ mr: '1rem' }} />
                     <ListItemText primary="Dashboard" />
                 </ListItemButton>
-                <ListItemButton component={NavLink} to="/accounts">
+                <ListItemButton
+                    component={NavLink}
+                    to="/accounts"
+                    selected={isLocation('/accounts')}
+                >
+                    <HandshakeOutlined sx={{ mr: '1rem' }} />
                     <ListItemText primary="Accounts" />
                 </ListItemButton>
                 <AdminOnlyWrapper>
-                    <ListItemButton component={NavLink} to="/users">
+                    <ListItemButton component={NavLink} to="/users" selected={isLocation('/users')}>
+                        <PeopleAltOutlined sx={{ mr: '1rem' }} />
                         <ListItemText primary="Users" />
                     </ListItemButton>
-                    <ListItemButton component={NavLink} to="/activitylog">
+                    <ListItemButton
+                        component={NavLink}
+                        to="/activitylog"
+                        selected={isLocation('/activitylog')}
+                    >
+                        <ListOutlined sx={{ mr: '1rem' }} />
                         <ListItemText primary="Activity Log" />
                     </ListItemButton>
                 </AdminOnlyWrapper>
             </nav>
-        </Box>
+        </Drawer>
     )
 }
--- Frontend/src/layout/AppLayout.tsx
+++ Frontend/src/layout/AppLayout.tsx
@@ -3,14 +3,14 @@ import { Navbar } from '../features/navigation/Navbar'
 import { Box, Button } from '@mui/material'
 import { useAuth } from '../features/auth/AuthProvider'
 
+const sidebarWidth = 240
+
 export const AppLayout = () => {
     const { logout } = useAuth()
 
     return (
         <Box sx={{ display: 'flex', minHeight: '100vh', width: '100%' }}>
-            <Box sx={{ width: 240 }}>
-                <Navbar />
-            </Box>
+            <Navbar width={sidebarWidth} />
             <Box sx={{ flex: 1, display: 'flex', p: '2rem' }}>
                 <Outlet />
             </Box>
