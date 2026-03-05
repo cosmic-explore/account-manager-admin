c89fc6fabab428ed39e98999faf4d0a0fcc6687a
cosmic-explore <nickwactor@gmail.com>
2026-03-04 17:15:53 -0700

    add DefaultPageCard

--- Frontend/src/layout/DefaultPage.tsx
+++ Frontend/src/layout/DefaultPage.tsx
@@ -4,9 +4,15 @@ import { Card, CardContent, Container } from '@mui/material'
 export const DefaultPage = ({ children }: { children: ReactNode }) => {
     return (
         <Container sx={{ p: '2rem' }}>
-            <Card variant="outlined" sx={{ p: '2rem' }}>
-                <CardContent>{children}</CardContent>
-            </Card>
+            <DefaultPageCard>{children}</DefaultPageCard>
         </Container>
+    )
+}
+
+export const DefaultPageCard = ({ children }: { children: ReactNode }) => {
+    return (
+        <Card variant="outlined" sx={{ p: '2rem' }}>
+            <CardContent>{children}</CardContent>
+        </Card>
     )
 }
