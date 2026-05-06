f63fe086be493443cbfa0059be17ee69f4bc38a6
cosmic-explore <nickwactor@gmail.com>
2026-05-06 15:39:36 -0600

    add backend endpoint to ping demo db

--- Backend/routes/demo.py
+++ Backend/routes/demo.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Response
+from sqlalchemy import text
 from constants import HTTP_POST
 from classes.base import db
 from services.seeding import reset_db
@@ -10,4 +11,13 @@ demo_bp = Blueprint("demo", __name__, url_prefix="/demo")
 def reset_demo_database():
     """Route that resets the database to a default state. Implemented for the app's live demo."""
     reset_db(db)
+    return Response(status=204)
+
+
+@demo_bp.route("/ping", methods=[HTTP_POST])
+def ping_database():
+    """Allows the demo database to be pinged to ensure that it's running"""
+    # only let the endpoint run if it's using the cron job's secret
+    # in a non-demo app, restrict access to this endpoint with a custom header
+    db.session.execute(text("SELECT 1"))
     return Response(status=204)
